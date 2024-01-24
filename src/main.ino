64644006eb779dd1b916b11d33c10efe1da67b5d
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2024-01-25 05:28:16 +0900

    MIDI音源からの発音に成功した

--- src/main.ino
+++ src/main.ino
@@ -2,19 +2,19 @@
 #include <SoftwareSerial.h>
 #define swrxPin 16
 #define swtxPin 17
-SoftwareSerial mySerial;
+// SoftwareSerial mySerial;
 // #include <SoftwareSerial.h>
 
-// SoftwareSerial softSerial(16,17);
+SoftwareSerial softSerial(swrxPin,swtxPin);
 // SoftwareSerial softSerial;
 
 
 // Create and bind the MIDI interface to the default hardware Serial port
 // MIDI_CREATE_DEFAULT_INSTANCE();
-MIDI_CREATE_INSTANCE(SoftwareSerial, mySerial, MIDI);
+MIDI_CREATE_INSTANCE(SoftwareSerial, softSerial, MIDI);
 
 void setup() {
-  mySerial.begin(9600, SWSERIAL_8N1, swrxPin, swtxPin , false, 256);
+//  mySerial.begin(9600, SWSERIAL_8N1, swrxPin, swtxPin , false, 256);
   MIDI.begin();
 //   Serial.begin(115200, SERIAL_8N1, 16, 17);// Grove
   // MIDI_CREATE_INSTANCE(SoftwareSerial, Serial2, midiInput);
@@ -23,11 +23,10 @@ void setup() {
 }
 
 void loop() {
-  if(mySerial.available()){
-     MIDI.sendNoteOn(42,127,1);  // ノートオン(pitch 42, velo 127 on channel 1)
-  }
+  MIDI.sendNoteOn(42,127,1);  // ノートオン(pitch 42, velo 127 on channel 1)
+
 //   MIDI.sendNoteOn(42,127,1);  // ノートオン(pitch 42, velo 127 on channel 1)
-//   delay(1000);                // 1秒待機
+   delay(1000);                // 1秒待機
 //   MIDI.sendNoteOff(42,0,1);   // ノートオフ
 //   delay(1000);                // 1秒待機
     // Send note 42 with velocity 127 on channel 1
