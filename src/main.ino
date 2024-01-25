2b0723b9eba94b44aed4b4dfa8a78223adcbec2e
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2024-01-25 13:12:33 +0900

    ESP-Nowの受信側コードに着手

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,6 @@
+#include <M5Unified.h>
+#include <WiFi.h>
+#include <esp_now.h>
 #include <MIDI.h>
 #include <SoftwareSerial.h>
 #define swrxPin 16
@@ -10,11 +13,32 @@ SoftwareSerial softSerial(swrxPin,swtxPin);
 // MIDI_CREATE_DEFAULT_INSTANCE();
 MIDI_CREATE_INSTANCE(SoftwareSerial, softSerial, MIDI);
 
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *payload, int data_len) {
+  Serial.print("Received Data: ");
+  String msg = String((char*) payload);
+  Serial.println(msg);
+};
+
 void setup() {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  M5.Display.setTextSize(3);               // テキストサイズを変更
+
+  // ESP-NOW初期化
+  WiFi.mode(WIFI_STA);
+  M5.Display.print("MAC Address: ");
+  M5.Display.println(WiFi.macAddress());
+  WiFi.disconnect();
+  if(esp_now_init() != ESP_OK) {
+    M5.Display.println("ESP-Now Init Failed....");
+    return;
+  } 
   MIDI.begin();
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
+  M5.update();
 //    MIDI.read();
   MIDI.sendNoteOn(42,127,1);  // ノートオン(pitch 42, velo 127 on channel 1)
    delay(1000);                // 1秒待機
